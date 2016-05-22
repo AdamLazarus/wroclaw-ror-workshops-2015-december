e75ae8eafa1149d8ce89be517507c9adee98f4d5
Adam Lazarus <adamlazarus92@gmail.com>
2016-05-22 11:51:17 +0800

    Configured routes for application

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  resources :students do
+
+  concern :subjectable do
     get :subjects
   end
+
+  resources :students, :teachers, concerns: :subjectable
+
+  namespace :reports, as: :report do
+    get :subjects
+  end
+
+  root "visitors#index"
 end
